7253fe43092a48acd5f4631af835fe9b00cd114c
Cloud <smirnovceo@yandex.ru>
2021-10-07 17:31:53 +0500

    Revert "Cleaning up the code\n\nRemoved: files for installing dependencies, unnecessary imports, comments and commented out code"
    This reverts commit 17adcfef0078f892f44054c63360635b0ce43b7a.

--- lesson_1.1/datacenter/passcard_info_view.py
+++ lesson_1.1/datacenter/passcard_info_view.py
@@ -1,10 +1,11 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils.timezone import timedelta
+from django.utils.timezone import localtime, timedelta
 
 
 def passcard_info_view(request, passcode):
+    # Программируем здесь
 
     def get_duration(visit):
         if visit.leaved_at:
@@ -37,10 +38,17 @@ def passcard_info_view(request, passcode):
 
     passcard = Passcard.objects.get(passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard.id)
-    this_passcard_visits = get_passcard_visits_context(visits)
 
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': get_passcard_visits_context(visits)
     }
     return render(request, 'passcard_info.html', context)
+
+# this_passcard_visits = [
+    #     {
+    #         'entered_at': '11-04-2018',
+    #         'duration': '25:03',
+    #         'is_strange': False
+    #     },
+    # ]
--- lesson_1.1/datacenter/storage_information_view.py
+++ lesson_1.1/datacenter/storage_information_view.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-
     all_visiters_inside = Visit.objects.filter(leaved_at=None)
 
     def get_duration(visiter):
@@ -34,6 +33,6 @@ def storage_information_view(request):
         )
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
     return render(request, 'storage_information.html', context)
